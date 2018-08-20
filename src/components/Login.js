a527e5074024cab81f3ec4bfa1b61c2af565df52
mariam <mariam2ac1@gmail.com>
2018-08-20 17:25:13 +0100

    Implement login using facebook

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,14 +2,24 @@ import React from 'react';
 import { Button } from 'react-native';
 
 export default class App extends React.Component {
-    onPressLearnMore() {
-        alert('ok')
+    async logIn() {
+      const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('2096112270413523', {
+          permissions: ['public_profile'],
+        });
+      if (type === 'success') {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(
+          `https://graph.facebook.com/me?access_token=${token}`);
+        alert(
+          `Hi ${(await response.json()).name}!`
+        );
+      }
     }
 
     render() {
       return (
         <Button
-        onPress={this.onPressLearnMore}
+        onPress={this.logIn}
         title="Sign in with facebook"
         color="#3c50e8"
         />
