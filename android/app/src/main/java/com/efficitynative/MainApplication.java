80378589ca4b7d6ad2868f6799167b46ba2d729c
mariam <mariam2ac1@gmail.com>
2018-08-26 13:38:30 +0100

    Link react native firebase

--- android/app/src/main/java/com/efficitynative/MainApplication.java
+++ android/app/src/main/java/com/efficitynative/MainApplication.java
@@ -3,6 +3,7 @@ package com.efficitynative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new FBSDKPackage(mCallbackManager)
       );
     }
