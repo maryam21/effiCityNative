cada45fc88ea3d5df69bd35bae86730cdbf6782d
mariam <mariam2ac1@gmail.com>
2018-08-27 10:58:01 +0100

    Link native base to android and ios

--- android/app/src/main/java/com/efficitynative/MainApplication.java
+++ android/app/src/main/java/com/efficitynative/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
@@ -32,6 +34,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
+            new FBSDKPackage(),
             new RNFirebasePackage(),
             new RNFirebaseAuthPackage(),
             new FBSDKPackage(mCallbackManager)
