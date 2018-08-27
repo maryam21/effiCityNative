30792742efbff016d00d62a45a3ea271c8e5f20a
mariam <mariam2ac1@gmail.com>
2018-08-27 12:12:22 +0100

    Rename card component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import FBLoginButton from './FBLoginButton';
-import ItemCard from './ItemCard';
+import ListingCard from './ListingCard';
 
 export default class App extends React.Component {
     render() {    
       return (
         <View>
             <FBLoginButton style={styles.login} />
-            <ItemCard />
+            <ListingCard />
         </View>
       );
     }
