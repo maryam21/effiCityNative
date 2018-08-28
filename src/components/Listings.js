4ebc1b6e0f55ff25ef4b418c349e41864813e71c
mariam <mariam2ac1@gmail.com>
2018-08-28 11:43:22 +0100

    Pass props to listing card component

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import { FlatList } from 'react-native';
 import ListingCard from './ListingCard';
 
 export default class Listings extends React.Component {
     render() {
       return (
         <FlatList
-            data={[{key: 'a'}, {key: 'b'}]}
-            renderItem={({item}) => <ListingCard/>}
+            data={[{key: 'App', description: 'blablal'}, {key: 'Chi', description: 'ooo'}]}
+            renderItem={({item}) => <ListingCard title={item.key} description={item.description}/>}
         />
       );
     }
