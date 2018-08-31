00db7655f022ef0ccab694359e21caec4fbf1ea5
mariam <mariam2ac1@gmail.com>
2018-08-31 17:53:53 +0100

    Manage item picker from home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { View, StyleSheet } from 'react-native';
 import FBLoginButton from './FBLoginButton';
 import Listings from './Listings';
 import ItemPicker from './ItemPicker';
+import PriceInput from './PriceInput';
 
 export default class Home extends React.Component {
   state = {
@@ -11,25 +12,27 @@ export default class Home extends React.Component {
     areas: ['AC Area', 'LO Area', 'Mon Area'],
     types: ['Flat', 'House']
   }
-
-  captureAreaValue = (value) => {
+  
+  onAreaChange = (value) => {
     this.setState({
       selectedArea: value
     })
   }
 
-  captureTypeValue = (value) => {
+  onTypeChange = (value) => {
     this.setState({
       selectedType: value
     })
   }
 
   render() {
+    alert(this.state.selectedType)
     return (
       <View style={styles.container} >
         <FBLoginButton />
-        <ItemPicker onChange={ e => { this.captureAreaValue(e) }} items={this.state.areas} />
-        <ItemPicker onChange={ e => { this.captureTypeValue(e) }} items={this.state.types} />
+        <ItemPicker onValueChange={this.onAreaChange} value={this.state.selectedArea} items={this.state.areas} />
+        <ItemPicker onValueChange={this.onTypeChange} value={this.state.selectedType} items={this.state.types} />
+        <PriceInput />
         <Listings selectedArea={this.state.selectedArea} selectedType={this.state.selectedType} />
       </View>
     );
