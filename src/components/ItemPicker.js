629939d1561efe83aff2e3fcf4ec8d6dc269c27b
mariam <mariam2ac1@gmail.com>
2018-09-03 18:05:11 +0100

    Use picker from react native library

--- src/components/ItemPicker.js
+++ src/components/ItemPicker.js
@@ -1,43 +1,17 @@
 import React, { Component } from "react";
-import { StyleSheet, View } from 'react-native';
-import { Container, Header, Content, Icon, Picker, Form } from "native-base";
+import { StyleSheet, View, Picker } from 'react-native';
 
 export default class ItemPicker extends Component {
-  constructor(props) {
-    super(props);
-    this.onValueChange = this.onValueChange.bind(this)
-    this.state = {
-      selected: undefined
-    };
-  }
-
-  onValueChange(value) {
-    this.setState({
-      selected: value
-    });
-  }
-
   render() {
     return (
       <View style={ styles.container }>
-      <Container>
-        <Content>
-          <Form>
-            <Picker
-              mode="dropdown"
-              iosIcon={<Icon name="ios-arrow-down-outline" />}
-              placeholder="Select your desired Area"
-              placeholderStyle={{ color: "#bfc6ea" }}
-              placeholderIconColor="#007aff"
-              style={{ width: undefined }}
-              selectedValue={this.props.value}
-              onValueChange={this.props.onValueChange}
-            >
-              {this.props.items.map((item) => <Picker.Item label={item} value={item} key={item} />)}
-            </Picker>
-          </Form>
-        </Content>
-      </Container>
+        <Picker
+          selectedValue={this.props.value}
+          style={{ height: 20, width: undefined, marginTop: 20 }}
+          onValueChange={this.props.onValueChange}
+        >
+          {this.props.items.map((item) => <Picker.Item label={item} value={item} key={item} />)}
+        </Picker>
       </View>
     );
   }
