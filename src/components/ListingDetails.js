1ab65bf1d0d520f74da6b4200c12ab44d4a81c67
mariam <mariam2ac1@gmail.com>
2018-09-03 11:30:17 +0100

    Add listing details component

---
+++ src/components/ListingDetails.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default class ListingDetails extends React.Component {
+    render() {
+      return (
+        <View style={ styles.container } >
+          <Text>{this.props.title}</Text>
+          <Text>{this.props.description}</Text>
+        </View>
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      flexDirection: 'column',
+      alignSelf: 'stretch',
+    },
+  });
+  
