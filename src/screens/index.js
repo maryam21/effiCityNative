0be7cccfbbba831601a6ce4d39f8b97a29265995
mariam <mariam2ac1@gmail.com>
2018-09-05 14:34:11 +0100

    Add chat screen

--- src/screens/index.js
+++ src/screens/index.js
@@ -3,10 +3,12 @@ import { Navigation } from 'react-native-navigation';
 import HomeScreen from '../components/Home';
 import ListingDetailsScreen from '../components/ListingDetails';
 import RegisterScreen from '../components/Register';
+import ChatScreen from '../components/Chat';
 
 // register all screens of the app (including internal ones)
 export function registerScreens() {
   Navigation.registerComponent('HomeScreen', () => HomeScreen);
   Navigation.registerComponent('ListingDetailsScreen', () => ListingDetailsScreen);
   Navigation.registerComponent('RegisterScreen', () => RegisterScreen);
+  Navigation.registerComponent('ChatScreen', () => ChatScreen);
 }
