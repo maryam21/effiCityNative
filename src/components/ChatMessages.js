624f3837407a6b899fa8c6f24aebc9ff8f1ef729
mariam <mariam2ac1@gmail.com>
2018-09-06 16:47:09 +0100

    Add method for fetching messages in chat messages component

--- src/components/ChatMessages.js
+++ src/components/ChatMessages.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
 import { Text, SectionList} from 'react-native';
+import firebase from 'react-native-firebase';
 
 class ChatMessages extends Component {
     state = {
-        text: null,
-        timestamp: null,
-        author: null,
+        data: []
+    }
+
+    getMessages () {
+        console.log(this.state.data)
+        const dbRef = firebase.database().ref('/chatrooms/' + this.props.chatroomId + '/messages/' );   
+        console.log(dbRef)
+        let items = [];
+        dbRef.on('value', function(snapshot) {
+            console.log('ol' + snapshot)
+            snapshot.forEach((child) => {
+                console.log(child)
+                items.push({
+                    key: child.key,
+                    author: child.val().author,
+                    text: child.val().text,
+                    timestamp: child.val().timestamp,
+                })
+            })
+            this.setState({
+                data: items
+            })
+        }, (err) => { console.log(err) })
+    }
+
+    componentWillUpdate() {
+        this.getMessages()
+        console.log(this.state.data)
+    }
+
+    componentDidMount () {
+        this.getMessages();
     }
 
     render() {
+        console.log(this.state.data)
         return (
             <SectionList
                 renderItem={({ item, index, section }) => <Text key={index}>{item}</Text>}
