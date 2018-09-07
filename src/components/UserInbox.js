3faff83353f0d0411f2c5374eb0411fd2073afb5
mariam <mariam2ac1@gmail.com>
2018-09-07 11:05:45 +0100

    Retrive current user from firbase by watching the auth state change

--- src/components/UserInbox.js
+++ src/components/UserInbox.js
@@ -4,34 +4,49 @@ import firebase from 'react-native-firebase';
 
 class UserInbox extends Component {
     state = {
-        data: ['ok']
+        data: [{
+            title: 'ho',
+            data: ['ok']
+        }]
     }
 
     getChatrooms = () => {
         const dbRef = firebase.database().ref('/chatrooms/');
-        const user = firebase.auth().currentUser;  
         let items = ['yu'];
+        console.log('opl')
 
-        dbRef.orderByChild('users').equalTo(user).on('value', (snapshot) => {
-            console.log(snapshot.val())
-            snapshot.forEach((child) => {
-                console.log(child)
-                /*items.push({
-                    /*title: child.val(),
-                    data: [
-                    child.val().messages,
-                    ]
-                })*/
-            })
+        firebase.auth().onAuthStateChanged(function(user) {
+            console.log('ui')
 
-            this.setState({
-                data: items
-            })
-        }, 
-        (err) => { console.log(err) }
-        )
-        
+            if (user) {
+                console.log('hoo')
+                dbRef.orderByChild('users').equalTo(user.uid).on('value', (snapshot) => {
+                    console.log(snapshot.val())
+                    snapshot.forEach((child) => {
+                        console.log(child)
+                        /*items.push({
+                            /*title: child.val(),
+                            data: [
+                            child.val().messages,
+                            ]
+                        })*/
+                    })
+                    /*
+                                this.setState({
+                                    data: items
+                                })*/
+                },
+                    (err) => { console.log(err) }
+                )
+            } else {
+                alert('you need to be logged in first')
+            }
+          });
     }
+
+    componentDidUpdate () {
+        this.getChatrooms();
+    } 
 
     componentDidMount () {
         this.getChatrooms();
