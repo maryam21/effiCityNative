6f27cc3260c39ccea85a85c4846626b123981234
mariam <mariam2ac1@gmail.com>
2018-09-09 13:09:23 +0100

    Javascript project setup for native navigation

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import { AppRegistry } from 'react-native';
+import Navigator from 'native-navigation';
 import App from './src/App';
-AppRegistry.registerComponent('effiCityNative', () => App);
+
+Navigator.registerScreen('effiCityNative', () => App);
