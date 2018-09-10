0c0ed13a1c3fcb445e9db9c66eb0b169815f7a59
mariam <mariam2ac1@gmail.com>
2018-09-10 12:42:13 +0100

    Update message input component to use text input element from native base

--- src/components/MessageInput.js
+++ src/components/MessageInput.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
+import { Container, Content, Item, Input } from 'native-base';
 
 class MessageInput extends Component {
     render() {
         return (
-            <TextInput
-                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                onChangeText={this.props.onChangeText}
-                value={this.props.text}
-            />
+            <Container>
+                <Content>
+                    <Item>
+                        <Input
+                            placeholder="Input your message"
+                            onChangeText={this.props.onChangeText}
+                            value={this.props.text}
+                        />
+                    </Item>
+                </Content>
+            </Container>
         );
     }
 }
