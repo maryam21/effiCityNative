096f17d80f80d5bd07669f615d334d097e8b0bf0
mariam <mariam2ac1@gmail.com>
2018-09-10 15:56:14 +0100

    Refactor max price input component

--- src/components/MaxPriceInput.js
+++ src/components/MaxPriceInput.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Content, Item, Input } from 'native-base';
 
-export default class MaxPriceInput extends Component {
-  render() {
-    return (
-      <Container>
-        <Content>
-          <Item>
-            <Input 
-              placeholder="Input max price"
-              onChangeText={this.props.onValueChange}
-              value={this.props.value}
-             />
-          </Item>
-        </Content>
-      </Container>
-    );
-  }
+MaxPriceInput = (props) => {
+  return (
+    <Container>
+      <Content>
+        <Item>
+          <Input
+            placeholder="Input max price"
+            onChangeText={props.onValueChange}
+            value={props.value}
+          />
+        </Item>
+      </Content>
+    </Container>
+  );
 }
 
 MaxPriceInput.propTypes = {
   value: PropTypes.number
 };
+
+export default MaxPriceInput;
