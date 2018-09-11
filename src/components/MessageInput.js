29d10101923c89359ebc75815cb8cf3b16fa728e
mariam <mariam2ac1@gmail.com>
2018-09-11 09:00:53 +0100

    Pass props to component

--- src/components/MessageInput.js
+++ src/components/MessageInput.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Container, Content, Item, Input } from 'native-base';
 
-const MessageInput = () => {
+const MessageInput = (props) => {
         return (
             <Container>
                 <Content>
                     <Item>
                         <Input
                             placeholder="Input your message"
-                            onChangeText={this.props.onChangeText}
-                            value={this.props.text}
+                            onChangeText={props.onChangeText}
+                            value={props.text}
                         />
                     </Item>
                 </Content>
